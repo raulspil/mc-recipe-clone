fd1613d5ad0cc8ea01a2a9d698f194012e2823da
Raul Spilca <raulspil@gmail.com>
2025-04-30 23:04:20 +0100

    add cache control configuration

--- next.config.js
+++ next.config.js
@@ -11,6 +11,20 @@ const nextConfig = {
   },
   // Add output configuration for serverless deployment
   output: 'standalone',
+  // Add headers configuration
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig; 
