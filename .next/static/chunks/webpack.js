c54ffb367b0cad48f08d40e4bbb5f62a78813ba0
Raul Spilca <raulspil@gmail.com>
2025-04-30 22:48:36 +0100

    fixing frontend

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e290aebacc4e22d"; }
+/******/ 		__webpack_require__.h = function() { return "394e0ddcde52416c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,9 @@ export default function Home() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+          'Pragma': 'no-cache',
+          'Expires': '0'
         },
         body: JSON.stringify({ url }),
       });
